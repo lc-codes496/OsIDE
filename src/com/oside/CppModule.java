bc2d28c16400030526ea6f95df744628aab41aa9
lc-codes496 <lucascavalnte2@gmail.com>
2025-10-05 18:24:07 -0300

    Create CppModule.java

---
+++ src/com/oside/CppModule.java
@@ -0,0 +1,28 @@
+package com.oside;
+
+import java.io.*;
+
+public class CppModule {
+
+    public static String compile(String sourceFile, String outputFile) {
+        return runCompiler("g++", sourceFile, "-o", outputFile);
+    }
+
+    private static String runCompiler(String... command) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.redirectErrorStream(true);
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            process.waitFor();
+            return output.toString();
+        } catch (Exception e) {
+            return "Erro ao compilar C++: " + e.getMessage();
+        }
+    }
+}
